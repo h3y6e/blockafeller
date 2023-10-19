52e211ddf44a4060c1e4298d802383d53c93e426
heyhoe <h3y6e@pm.me>
2023-10-20 04:42:57 +0900

    fix lints

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-import { watch } from "chokidar";
 import { exec } from "child_process";
 import { promisify } from "util";
+import { watch } from "chokidar";
 
 async function runCommand(command: string) {
-	try {
-		await promisify(exec)(command);
-	} catch (e) {
-		// biome-ignore lint/suspicious/noExplicitAny
-		console.error((e as any).stderr.toString());
-	}
+  try {
+    await promisify(exec)(command);
+  } catch (e) {
+    // biome-ignore lint/suspicious/noExplicitAny: any is fine here
+    console.error((e as any).stderr.toString());
+  }
 }
 
 watch("**/*.ts", { ignored: "index.ts" }).on("change", (path) => {
   console.log(`Building ${path}...`);
-	// Bun currently supports only esm @see https://bun.sh/docs/bundler#format
-	// Bun.build({
-	// 	entrypoints: [path],
+  // Bun currently supports only esm @see https://bun.sh/docs/bundler#format
+  // Bun.build({
+  // 	entrypoints: [path],
   //   outdir: path.slice(0, path.lastIndexOf("/")),
   //   format: "cjs",
   // });
-	const js = `${path.slice(0, path.lastIndexOf("."))}.js`;
+  const js = `${path.slice(0, path.lastIndexOf("."))}.js`;
   runCommand(`bunx swc ${path} -o ${js}`);
-	runCommand(`bunx jscad ${js}`);
+  runCommand(`bunx jscad ${js}`);
 });
