6847fa106ca91dc13e60b99a10cc8b8f2bedb4a2
heyhoe <h3y6e@pm.me>
2023-10-20 04:11:10 +0900

    laid dev groundwork

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # blockafeller
+
+```bash
+bun index.ts
+```
+
+## Acknowledgements
+
+- https://github.com/mtsmfm/things by [mtsmfm](https://github.com/mtsmfm/)  
+  I referred to some code snippets from this repository. The original code is licensed under the [MIT License](https://github.com/mtsmfm/things/blob/main/LICENSE).
+
+## License
+
+[MIT License](LICENSE)
---
+++ index.ts
@@ -0,0 +1,25 @@
+import { watch } from "chokidar";
+import { exec } from "child_process";
+import { promisify } from "util";
+
+async function runCommand(command: string) {
+	try {
+		await promisify(exec)(command);
+	} catch (e) {
+		// biome-ignore lint/suspicious/noExplicitAny
+		console.error((e as any).stderr.toString());
+	}
+}
+
+watch("**/*.ts", { ignored: "index.ts" }).on("change", (path) => {
+  console.log(`Building ${path}...`);
+	// Bun currently supports only esm @see https://bun.sh/docs/bundler#format
+	// Bun.build({
+	// 	entrypoints: [path],
+  //   outdir: path.slice(0, path.lastIndexOf("/")),
+  //   format: "cjs",
+  // });
+	const js = `${path.slice(0, path.lastIndexOf("."))}.js`;
+  runCommand(`bunx swc ${path} -o ${js}`);
+	runCommand(`bunx jscad ${js}`);
+});
