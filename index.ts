29e94669b22578064ea53c01d000c77cd18f10d7
heyhoe <h3y6e@pm.me>
2023-10-22 05:56:09 +0900

    update script

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { exec } from "child_process";
+import { ExecException, exec } from "child_process";
 import { promisify } from "util";
 import { watch } from "chokidar";
 
@@ -6,13 +6,33 @@ async function runCommand(command: string) {
   try {
     await promisify(exec)(command);
   } catch (e) {
-    // biome-ignore lint/suspicious/noExplicitAny: any is fine here
-    console.error((e as any).stderr.toString());
+    const error = e as ExecException & { stderr?: string };
+    if (error.stderr) {
+      console.error(error.stderr);
+    } else {
+      console.error(error.message);
+    }
   }
 }
 
-watch("**/*.ts", { ignored: "index.ts" }).on("change", (path) => {
-  console.log(`Building ${path}...`);
+function resetCursor() {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+}
+
+function displaySpinner(interval: number) {
+  const spinner = ["|", "/", "-", "\\"];
+  let current = 0;
+  const handle = setInterval(() => {
+    resetCursor();
+    process.stdout.write(`${spinner[current]} Building...`);
+    current = (current + 1) % spinner.length;
+  }, interval);
+  return handle;
+}
+
+watch("**/*.ts", { ignored: "index.ts" }).on("change", async (path) => {
+  console.log(`\x1b[1m${path}\x1b[0m`);
   // Bun currently supports only esm @see https://bun.sh/docs/bundler#format
   // Bun.build({
   // 	entrypoints: [path],
@@ -20,6 +40,10 @@ watch("**/*.ts", { ignored: "index.ts" }).on("change", (path) => {
   //   format: "cjs",
   // });
   const js = `${path.slice(0, path.lastIndexOf("."))}.js`;
-  runCommand(`bunx swc ${path} -o ${js}`);
-  runCommand(`bunx jscad ${js}`);
+  const handle = displaySpinner(100);
+  await runCommand(`bunx swc ${path} -o ${js}`);
+  await runCommand(`bunx jscad ${js}`);
+  clearInterval(handle);
+  resetCursor();
+  console.log("Building...\nDone!");
 });
