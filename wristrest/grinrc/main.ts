af3e52c4e2cddb453d989e5785c7d5e9a009feaf
heyhoe <h3y6e@pm.me>
2023-11-09 04:34:21 +0900

    Add bulge to wristrest and split into two parts

--- wristrest/grinrc/main.ts
+++ wristrest/grinrc/main.ts
@@ -1,43 +1,56 @@
-import { booleans, extrusions, geometries, primitives, transforms } from "@jscad/modeling";
+import { booleans, extrusions, geometries, hulls, primitives, transforms } from "@jscad/modeling";
 import type { Vec2 } from "@jscad/modeling/src/maths/vec2";
-import expansions from "@jscad/modeling/src/operations/expansions";
 
 const leftLength = 140;
 const rightLength = 160;
+const length = leftLength + rightLength;
+const lengthCenter = (leftLength - rightLength) / 2;
 const width = 70;
 const height = 8;
 
 export const main = () => {
   // 基本のリストレストの3D形状
-  const base = primitives.roundedCuboid({
-    center: [(leftLength - rightLength) / 2, -width / 2, 0],
+  const base = primitives.cuboid({
+    center: [lengthCenter, -width / 2, height / 2],
     size: [leftLength + rightLength, width, height],
-    roundRadius: 2,
   });
 
   // 2D曲線
   const y = [0, -0.5, -1.3, -3.1, -5.7, -9.1, -12, -14.5, -16, -17, -17.5];
   const curvePoints: Vec2[] = [
     [-rightLength, 0],
-    ...Array.from({ length: 21 }, (_, i): Vec2 => [-100 + i * 10, i < 11 ? y[i] : y[20 - i]]),
+    ...Array.from({ length: y.length * 2 - 1 }, (_, i): Vec2 => {
+      const index = i < y.length ? i : y.length * 2 - 2 - i;
+      return [-100 + i * 10, y[index]];
+    }),
     [leftLength, 0],
   ];
-  const curvePath = geometries.path2.fromPoints({ closed: true }, curvePoints);
+
+  const curveGeom = geometries.geom2.fromPoints(curvePoints);
 
   // 曲線を3D形状に押し出し
-  const curve = extrusions.extrudeLinear({ height: height }, curvePath);
-  const positionedCurve = transforms.translate([0, 0, -height / 2], curve);
+  const curve = extrusions.extrudeLinear({ height: height }, curveGeom);
 
-  // 周囲を整形する円
-  const circle = primitives.cylinder({
-    radius: 158,
-    height: height,
-    center: [-11, 51, 0],
-    segments: 64,
+  // 膨らみ
+  const cuboid = primitives.cuboid({
+    center: [lengthCenter, 0, height / 2],
+    size: [leftLength + rightLength, width / 2, height],
+  });
+  const bulge = booleans.intersect(cuboid, curve);
+
+  // リストレストの3D形状
+  const wristrest = booleans.union(booleans.subtract(base, curve), transforms.translate([0, -width, 0], bulge));
+
+  // オブジェクトを分割するための直方体を作成
+  const cutter = primitives.cuboid({
+    center: [length / 4 + lengthCenter, -width / 2, height / 2],
+    size: [rightLength, 2 * width, height],
   });
 
-  const wristrest = booleans.intersect(booleans.subtract(base, positionedCurve), circle);
+  // オブジェクトを分割する
+  const rightPart = booleans.intersect(wristrest, cutter);
+  const leftPart = booleans.subtract(wristrest, cutter);
 
-  return wristrest;
-  // return expansions.expand({ corners: "round", delta: 0.5 }, wristrest);
+  // 分割された2つの部分を返す
+  return [rightPart, transforms.translate([length / 2, 90, 0], leftPart)];
 };
