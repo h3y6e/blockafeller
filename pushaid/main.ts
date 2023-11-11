26450c6e12953e162a545995eaab7e4d31b8f0e1
heyhoe <h3y6e@pm.me>
2023-11-11 23:12:50 +0900

    update shape with rounded  and adjust dimensions

--- pushaid/main.ts
+++ pushaid/main.ts
@@ -4,12 +4,13 @@ export const main = () => {
   const length = 50;
   const width = 15;
   const height = 5;
-  const radius = 5;
-  const depth = 2;
+  const radius = 5.5;
+  const depth = 1;
 
-  const base = primitives.cuboid({
+  const base = primitives.roundedCuboid({
     size: [length, width, height],
     center: [0, 0, height / 2],
+    roundRadius: 1,
   });
   const circle = primitives.cylinder({
     height: 2 * depth,
